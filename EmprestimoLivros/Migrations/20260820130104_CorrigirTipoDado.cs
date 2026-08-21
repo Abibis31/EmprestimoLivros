5214088c6079c5a56a2763f77ab085a8386374b9
abimael.borges <abimael.borges@FECN-FLN3246>
2026-08-21 15:06:29 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Migrations/20260820130104_CorrigirTipoDado.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmprestimoLivros.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrigirTipoDado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Recebedor",
+                table: "Emprestimos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LivroEmprestado",
+                table: "Emprestimos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Forcenedor",
+                table: "Emprestimos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Recebedor",
+                table: "Emprestimos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LivroEmprestado",
+                table: "Emprestimos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Forcenedor",
+                table: "Emprestimos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Forcenedor { get; set; }
+        public string LivroEmprestado { get; set; }
+
+        public DateTime dataUltimaAtualização { get; set; } = DateTime.Now;
+    } 
+}
