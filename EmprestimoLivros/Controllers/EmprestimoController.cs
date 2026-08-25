fa6fe769b2c6c85cdd831d600cf62c1aa99a58e0
Abibis31 <abimaelvieirawelwel@gmail.com>
2026-08-25 15:17:52 -0300

    Feito a criação do botão de exportação para xml e correção da data de cadastro do empréstimo

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -1,6 +1,8 @@
 ﻿using EmprestimoLivros.Data;
 using EmprestimoLivros.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using ClosedXML.Excel;
 
 namespace EmprestimoLivros.Controllers
 {
@@ -56,12 +58,53 @@ namespace EmprestimoLivros.Controllers
 
             return View(emprestimo);
         }
+        [HttpGet]
+        public IActionResult Exportar() 
+        {
+            var dados = GetDados();
+
+            using (XLWorkbook workBook = new XLWorkbook()) 
+            {
+                workBook.AddWorksheet(dados,"Dados empréstimos");
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Emprestimos.xlsx");
+                }
+            }
+            
+        }
+
+        private DataTable GetDados()
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable.TableName= "Dados empréstimos";
+
+            dataTable.Columns.Add("Recebedor", typeof(string));
+            dataTable.Columns.Add("Fornecedor", typeof(string));
+            dataTable.Columns.Add("Livro", typeof(string));
+            dataTable.Columns.Add("Data empréstimo", typeof(DateTime));
+
+            var dados = _db.Emprestimos.ToList();
+
+            if(dados.Count > 0) {
+                foreach (var item in dados)
+                {
+                    dataTable.Rows.Add(item.Recebedor, item.Forcenedor, item.LivroEmprestado, item.dataUltimaAtualização);
+                }
+            }   
+            return dataTable;
+
+        }
 
         [HttpPost]
         public IActionResult Cadastrar(EmprestimosModel emprestimos)
         {
             if (ModelState.IsValid)
             {
+                emprestimos.dataUltimaAtualização = DateTime.Now;
                 _db.Emprestimos.Add(emprestimos);
                 _db.SaveChanges();
 
@@ -76,7 +119,13 @@ namespace EmprestimoLivros.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Emprestimos.Update(emprestimos);
+                var emprestimoDB = _db.Emprestimos.Find(emprestimos.Id);
+                emprestimoDB.Forcenedor = emprestimos.Forcenedor;
+                emprestimoDB.Recebedor = emprestimos.Recebedor;
+                emprestimoDB.LivroEmprestado = emprestimos.LivroEmprestado;
+                
+
+                _db.Emprestimos.Update(emprestimoDB);
                 _db.SaveChanges();
                 TempData["MensagemSucesso"] = "Empréstimo atualizado com sucesso!";
                 return RedirectToAction("Index");
